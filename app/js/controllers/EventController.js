b7367d673e345747e1635d7547022acec60ffd36
Matt Payne <Payne@MattPayne.org>
2017-09-22 18:32:49 -0500

    Adding data and formatting.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -6,7 +6,12 @@ eventsApp.controller('EventController',
         $scope.event = {
             name: 'Angular Boot Camp',
             date: '1/1/2013',
-            time: '10:30 am'
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            }
         };
 
     }
