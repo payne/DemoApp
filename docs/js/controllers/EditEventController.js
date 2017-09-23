ede14f2bfcdbb7b25d283581e65ccb70f4f9278d
Matt Payne <Payne@MattPayne.org>
2017-09-23 18:12:24 -0500

    Simple ng-click functions.

--- docs/js/controllers/EditEventController.js
+++ docs/js/controllers/EditEventController.js
@@ -1,7 +1,13 @@
 'use strict';
 
 eventsApp.controller('EditEventController',
-   function EditEventController($scope) {
+    function EditEventController($scope) {
+        $scope.saveEvent = function(event) {
+            window.alert('Event ' + event.name + ' saved!');
+        };
 
-   }
-);
+        $scope.cancelEvent = function() {
+            window.location="EventDetails.html";
+        }
+    }
+)
