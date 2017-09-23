8585e663582866fa305b80eec1d1a636ad44cc48
Matt Payne <Payne@MattPayne.org>
2017-09-22 19:13:06 -0500

    ng-bind-html and ng-show and ng-hide

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,7 +3,8 @@
 eventsApp.controller('EventController',
     function EventController($scope) {
 
-        $scope.snippet = '<span style="color:red;">hi there</span>';
+        $scope.snippet = '<span style="color:red">hi there</span>';
+        $scope.boolValue = true;
         $scope.event = {
             name: 'Angular Boot Camp',
             date: '1/1/2013',
