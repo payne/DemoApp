edba34de1bfb35d4b67555a5fbe7e71599c8650c
Matt Payne <Payne@MattPayne.org>
2017-09-23 18:25:33 -0500

    Demo styling with angular form classes.

--- docs/js/controllers/EditEventController.js
+++ docs/js/controllers/EditEventController.js
@@ -3,6 +3,7 @@
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
         $scope.saveEvent = function(event, newEventForm) {
+            console.log(newEventForm);
             if (newEventForm.$valid) {
                 window.alert('Event ' + event.name + ' saved!');
             }
