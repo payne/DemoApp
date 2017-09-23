80b74944927322abfcd03b2eea9e519e6e16798a
Matt Payne <Payne@MattPayne.org>
2017-09-22 19:43:10 -0500

    Custom filter example.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -20,7 +20,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'Bob Smith',
-                    duration: '1 hr',
+                    duration: 1,
                     level: 'Advanced',
                     abstract: 'In this session you will learn the ins and outs of directives.',
                     upVoteCount: 0
@@ -28,7 +28,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
-                    duration: '30 minutes',
+                    duration: 2,
                     level: 'Introductory',
                     abstract: 'This session will take a closer look at scopes...',
                     upVoteCount: 0
@@ -36,7 +36,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
-                    duration: '2 hours',
+                    duration: 4,
                     level: 'Intermediate',
                     abstract: 'Controllers are the beginning of everthing Angular',
                     upVoteCount: 0
