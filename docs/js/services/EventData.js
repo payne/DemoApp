1d507fb19f1154db297943929bd852ac8a5c85a4
Matt Payne <Payne@MattPayne.org>
2017-09-24 14:28:15 -0500

    typo fix

--- docs/js/services/EventData.js
+++ docs/js/services/EventData.js
@@ -2,7 +2,6 @@ eventsApp.factory('eventData', function ($resource) {
     return {
         getEvent: function() {
             return $resource('data/event/:id', { id: '@id'}).get({id:1});
-            return $http({method: 'GET', url: 'data/event/1.json'});
         }
     };
 });
