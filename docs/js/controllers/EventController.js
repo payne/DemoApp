6f0491f2f7947fbdb06667938deb7d0b04490f33
Matt Payne <Payne@MattPayne.org>
2017-09-24 14:25:35 -0500

    Using $promise of a resource.

--- docs/js/controllers/EventController.js
+++ docs/js/controllers/EventController.js
@@ -6,7 +6,12 @@ eventsApp.controller('EventController',
         //$scope.snippet = '<span style="color:red">hi there</span>';
         $scope.boolValue = true;
         $scope.mystyle = {color: 'red'};
-        $scope.event = eventData.getEvent();
+        // $scope.event = eventData.getEvent();
+        $scope.event = eventData.getEvent()
+            .$promise.then(
+                function(event) { $scope.event = event; console.log(event); },
+                function(response) { $console.log(response); }
+            );
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
