e738d24c67773637dfae4093d48a5dca80898e50
Matt Payne <Payne@MattPayne.org>
2017-09-24 14:27:34 -0500

    More semantic way of using promise

--- docs/js/controllers/EventController.js
+++ docs/js/controllers/EventController.js
@@ -8,10 +8,9 @@ eventsApp.controller('EventController',
         $scope.mystyle = {color: 'red'};
         // $scope.event = eventData.getEvent();
         $scope.event = eventData.getEvent()
-            .$promise.then(
-                function(event) { $scope.event = event; console.log(event); },
-                function(response) { console.log(response); }
-            );
+            .$promise
+            .then( function(event) { $scope.event = event; console.log(event); })
+            .catch(function(response) { console.log(response); });
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
