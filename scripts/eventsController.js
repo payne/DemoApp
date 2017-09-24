ac134d8e60bd31887010f6c6ca083a34df986bd3
Matt Payne <Payne@MattPayne.org>
2017-09-24 14:54:56 -0500

    $resource to save to simple express server.

---
+++ scripts/eventsController.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+module.exports.get = function(req, res) {
+    var event = fs.readFileSync('docs/data/event/' + req.params.id + '.json', 'utf8');
+    res.setResponseHeaders('Content-Type', 'application/json');
+    res.send(event);
+};
+
+module.exports.save = function(req, res) {
+    var event = req.body;
+    fs.writeFileSync('docs/data/event' + req.params.id + '.json', JSON.stringify(event));
+    res.send(event);
+};
