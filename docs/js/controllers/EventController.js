a98058394352695bf9bba216e8ca8bebea58f306
Matt Payne <Payne@MattPayne.org>
2017-09-24 14:22:01 -0500

    Switched to $resource from $http

--- docs/js/controllers/EventController.js
+++ docs/js/controllers/EventController.js
@@ -6,11 +6,7 @@ eventsApp.controller('EventController',
         //$scope.snippet = '<span style="color:red">hi there</span>';
         $scope.boolValue = true;
         $scope.mystyle = {color: 'red'};
-        $scope.event = eventData.getEvent()
-            .success(function(event) { $scope.event = event; })
-            .error(function(data, status, headers, config) {
-                $log.warn(data, status, headers(), config);
-            });
+        $scope.event = eventData.getEvent();
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
