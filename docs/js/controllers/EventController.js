2f3d7968b19ba5a04c75f8d292ddea7e4fb44485
Matt Payne <Payne@MattPayne.org>
2017-09-24 14:07:35 -0500

    EventData server now returns a promise to EventController.

--- docs/js/controllers/EventController.js
+++ docs/js/controllers/EventController.js
@@ -1,14 +1,16 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData) {
+    function EventController($scope, eventData, $log) {
 
         //$scope.snippet = '<span style="color:red">hi there</span>';
         $scope.boolValue = true;
         $scope.mystyle = {color: 'red'};
-        $scope.event = eventData.getEvent(function(event) {
-            $scope.event = event;
-        });
+        $scope.event = eventData.getEvent()
+            .success(function(event) { $scope.event = event; })
+            .error(function(data, status, headers, config) {
+                $log.warn(data, status, headers(), config);
+            });
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
